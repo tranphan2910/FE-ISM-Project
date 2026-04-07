69b9878f67e824ffad03eaf07c479059faa76880
Tran Phan <tranphan2910@gmail.com>
2026-04-07 22:35:21 +0700

    add antd

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src'),
+    },
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
