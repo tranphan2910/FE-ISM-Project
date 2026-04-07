e6931a4d33b2a6156ed88baae478a1622350c135
Tran Phan <tranphan2910@gmail.com>
2026-04-07 23:29:49 +0700

    add register page

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
+import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+  },
   resolve: {
     alias: {
       '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), './src'),
